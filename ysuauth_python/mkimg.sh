ea2f5d0519b0dbc0378b8388925f66bdca149df7
Haomin Kong <a645162@qq.com>
2021-08-15 20:49:38 +0800

    修复armhf架构下的树莓派系统无法正常使用的问题。

--- ysuauth_python/mkimg.sh
+++ ysuauth_python/mkimg.sh
@@ -5,9 +5,9 @@ docker buildx install
 # alpine支持的所有platform我都支持一下吧(3.9.6-alpine3.13有linux/s390x而3.14就没了，就不支持了)
 
 docker buildx build --push \
-  --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6,linux/ppc64le \
-  -t a645162/ysuauth:3.0.1 .
+  --platform linux/amd64,linux/arm64,linux/386,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
+  -t a645162/ysuauth:3.0.2 .
 
-#docker build -t a645162/ysuauth:3.0 .
+#docker build -t ysuauth-dev:3.0.2 .
 
 #docker buildx build -t yangchuansheng/hello-arch --platform=linux/arm,linux/arm64,linux/amd64 . --push
